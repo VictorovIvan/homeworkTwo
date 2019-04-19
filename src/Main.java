6d7e9b198839f2a1fd19bc3cac1ab1bf400ddf7e
VictorovIvan <victorov.i.vl@outlook.com>
2019-04-20 00:42:09 +0300

    Added .xml + .iml

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,29 @@
 import java.util.Arrays;
 
-import static java.lang.System.currentTimeMillis;
-
-public class
-Main {
+/**
+ * Main class
+ */
+public class Main {
+    /**
+     * Main for testing
+     */
     public static void main(String[] args) {
         TaskOne modelError = new TaskOne();
         TaskTwo random = new TaskTwo();
-        TaskThree.Person sort[];
+        TaskThree.Person sortObj[];
         long reamainTime;
 
+        sortObj = TaskThree.Person.generateSomethingObjects(1000);
 
-        sort = TaskThree.Person.generateSomethingObjects(1000);
+        modelError.modelNullPointerException();
+        modelError.modelArrayIndexOutOfBoundsException();
+        modelError.modelThrowMyException();
 
-//        modelError.modelNullPointerException();
-//        modelError.modelArrayIndexOutOfBoundsException();
-//        modelError.modelThrowMyException();
-
-//        random.genRandomNumb(5);
-
+        random.genRandomNumb(5);
 
         System.out.println("Время сортировки: " +
-                           TaskThree.Person.sortComparatorWithCalcTime(sort, TaskThree.Person.CompareToAgeToSexToName)
-                           + " MiliSecunds");
-        System.out.println("Cортировка:\n" + Arrays.toString(sort));
-
+                TaskThree.Person.sortComparatorWithCalcTime(sortObj, TaskThree.Person.CompareToAgeToSexToName)
+                + " MiliSecunds");
+        System.out.println("Cортировка:\n" + Arrays.toString(sortObj));
     }
-
-
 }
